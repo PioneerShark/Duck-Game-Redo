898f3db0fbf4a67c991788cd9b8cc5f26c1fe6a1
PioneeShark <wuyi@mail.com>
2024-06-26 12:42:12 +0100

    Move Method + Assets

---
+++ Assets/Scripts/Entity/TestEntity.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestEntity : BaseEntity
+{
+    // Start is called before the first frame update
+   public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        base.Update();
+    }
+    public override void Move(int moveX, int moveY)
+    {
+        base.Move(moveX, moveY);
+    }
+}
