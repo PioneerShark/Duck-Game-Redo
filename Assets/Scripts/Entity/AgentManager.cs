294489715b7fa677ce99ba9f75b25a1d4795089f
Alan Robinson <71782048+KingNanachi@users.noreply.github.com>
2024-07-01 17:40:36 +0100

    Agent Foundation 1

---
+++ Assets/Scripts/Entity/AgentManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    private List<Agent> agents = new List<Agent>();
+
+    public void CreateAgent()
+    {
+        
+    }
+
+    public void AddAgent()
+    {
+
+    }
+}
